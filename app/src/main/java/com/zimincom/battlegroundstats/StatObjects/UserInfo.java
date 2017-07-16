46bab563d3dd2187d392c7bb65bd239146552e10
지민 <wlals822@naver.com>
2017-07-16 14:54:55 +0900

    Add stat objects

---
+++ app/src/main/java/com/zimincom/battlegroundstats/StatObjects/UserInfo.java
@@ -0,0 +1,21 @@
+package com.zimincom.battlegroundstats.StatObjects;
+
+/**
+ * Created by Zimincom on 2017. 7. 16..
+ */
+
+public class UserInfo {
+    private Integer platformId;
+    private String AccountId;
+    //link of avatar
+    private String Avatar;
+    private String selectedRegion;
+    private String defaultSeason;
+    private String seasonDisplay;
+    private String LastUpdated;
+    private String LiveTracking;
+    private String Playername;
+    private int PubgTrackerId;
+    private Stat[] Stats;
+
+}
