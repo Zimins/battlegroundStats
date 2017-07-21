c15cce4f8d7171add02146242da69843fb6357e5
지민 <wlals822@naver.com>
2017-07-21 16:08:18 +0900

    retrofit을 통한 json 수신 ok
    * Logger.json 작동 안됨 : 길이 문제인듯

--- app/src/main/java/com/zimincom/battlegroundstats/MainActivity.java
+++ app/src/main/java/com/zimincom/battlegroundstats/MainActivity.java
@@ -1,7 +1,20 @@
 package com.zimincom.battlegroundstats;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.orhanobut.logger.Logger;
+import com.zimincom.battlegroundstats.StatObjects.UserInfo;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.zimincom.battlegroundstats.RemoteService.BASE_URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Gson gson = new Gson();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        RemoteService pubgService = retrofit.create(RemoteService.class);
+
+        Call<UserInfo> call = pubgService.getUserInfo("Killer_lim");
+        call.enqueue(new Callback<UserInfo>() {
+            @Override
+            public void onResponse(Call<UserInfo> call, Response<UserInfo> response) {
+               if (response.isSuccessful()) {
+                   UserInfo userInfo = response.body();
+                   Logger.json(userInfo.toString());
+               } else {
+                   Log.d("main", "error");
+               }
+            }
+
+            @Override
+            public void onFailure(Call<UserInfo> call, Throwable t) {
+                Log.d("main", "onFailure");
+                t.printStackTrace();
+            }
+        });
     }
 }
