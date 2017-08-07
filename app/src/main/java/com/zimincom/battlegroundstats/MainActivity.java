2a4891a8cd90d58ff2bcf7deb65236cb1c1db161
지민 <wlals822@naver.com>
2017-08-07 19:56:53 +0900

    툴바 추가하여 검색기능 추가
    * 이름 안입력할때 , 없을 때 등 처리

--- app/src/main/java/com/zimincom/battlegroundstats/MainActivity.java
+++ app/src/main/java/com/zimincom/battlegroundstats/MainActivity.java
@@ -3,12 +3,21 @@ package com.zimincom.battlegroundstats;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.orhanobut.logger.AndroidLogAdapter;
 import com.orhanobut.logger.Logger;
+import com.squareup.picasso.Picasso;
 import com.zimincom.battlegroundstats.StatObjects.History;
 import com.zimincom.battlegroundstats.StatObjects.LiveTracking;
 import com.zimincom.battlegroundstats.StatObjects.Stat;
@@ -28,12 +37,20 @@ public class MainActivity extends AppCompatActivity {
     String userNickName = "";
     RemoteService pubgService;
 
+    ImageView profileImage;
+    TextView nicknameTextView;
     TextView result;
+    EditText nickNameInput;
+    ProgressBar searchProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         Gson gson = new Gson();
 
@@ -45,7 +62,11 @@ public class MainActivity extends AppCompatActivity {
                 .build();
 
         pubgService = retrofit.create(RemoteService.class);
+        profileImage = (ImageView) findViewById(R.id.iv_profile);
+        nicknameTextView = (TextView) findViewById(R.id.tv_nickname);
         result = (TextView) findViewById(R.id.tv_response);
+        nickNameInput = (EditText) findViewById(R.id.input_nickname);
+        searchProgressBar = (ProgressBar) findViewById(R.id.progress_search);
 
         // TODO: 2017. 7. 30. 로딩바 추가하기
 
@@ -63,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void requestStats(String userNickName) {
+    private void requestStats(final String userNickName) {
 
         Call<UserInfo> call = pubgService.getUserInfo(userNickName);
         call.enqueue(new Callback<UserInfo>() {
@@ -71,17 +92,29 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<UserInfo> call, Response<UserInfo> response) {
                 if (response.isSuccessful()) {
                     UserInfo userInfo = response.body();
-                    Log.d("main", userInfo.toString());
                     Logger.json(response.body().toString());
-                    LiveTracking[] liveTracking = userInfo.getLiveTracking();
-                    History[] histories = userInfo.getHistories();
-                    Stat[] stats = histories[0].getStats();
-                    Logger.d(liveTracking);
-                    Logger.d(histories[0]);
-                    Logger.d(stats[17]);
-                    result.setText(userInfo.toString());
+                    if (userInfo.getPlayerName() == null) {
+                        Toast.makeText(MainActivity.this, "result not found", Toast.LENGTH_LONG)
+                                .show();
+                        nickNameInput.setText("");
+                    } else {
+                        LiveTracking[] liveTracking = userInfo.getLiveTracking();
+                        History[] histories = userInfo.getHistories();
+                        Stat[] stats = histories[0].getStats();
+                        Picasso.with(MainActivity.this)
+                                .load(userInfo.getAvatarImageUrl())
+                                .into(profileImage);
+                        nicknameTextView.setText(userInfo.getPlayerName());
+                        result.setText(userInfo.toString());
+                    }
+
+                    if (searchProgressBar.getVisibility() == View.VISIBLE) {
+                        searchProgressBar.setVisibility(View.INVISIBLE);
+                    }
+
+
                 } else {
-                    Log.d("main", "error");
+                    Logger.d("response is not successful");
                 }
             }
 
@@ -91,5 +124,20 @@ public class MainActivity extends AppCompatActivity {
                 t.printStackTrace();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_search) {
+            searchProgressBar.setVisibility(View.VISIBLE);
+            requestStats(nickNameInput.getText().toString());
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
