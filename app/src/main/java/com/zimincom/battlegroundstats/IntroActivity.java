28ef7a118a888a4dbeeb290e4ed45b89f3e1928c
Myoung-jae-Kim <rlaaud30@naver.com>
2017-08-27 14:36:36 +0900

    List View and Text Color (#11)
    * List View and Text Color
    
    * Color fix

--- app/src/main/java/com/zimincom/battlegroundstats/IntroActivity.java
+++ app/src/main/java/com/zimincom/battlegroundstats/IntroActivity.java
@@ -6,15 +6,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.os.Build;
+import android.widget.ListView;
 
 public class IntroActivity extends Activity {
 
-    EditText nickNameInput;
+    //EditText nickNameInput;
+    AutoCompleteTextView nickNameInput;
     Button buttonSearch;
     String nickName = null;
+    static final String[] listMenu = {"LIST1", "LIST2", "LIST3"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +33,10 @@ public class IntroActivity extends Activity {
         setContentView(R.layout.activity_intro);
 
         buttonSearch = (Button) findViewById(R.id.button_search);
-        nickNameInput = (EditText) findViewById(R.id.nickNameInput);
+        nickNameInput = (AutoCompleteTextView) findViewById(R.id.nickNameInput);
 
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, listMenu);
+        nickNameInput.setAdapter(adapter);
 
         buttonSearch.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
